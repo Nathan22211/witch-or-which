e76505fe3994dc5b38ac38000e710d15b3a88930
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2024-10-19 16:34:58 -0500

    Add files via upload

--- witch-or-which/player.gd
+++ witch-or-which/player.gd
@@ -1,7 +1,7 @@
 extends Area2D
 signal hit
 
-@export var speed = 100 # How fast the player will move (pixels/sec).
+@export var speed = 200 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
