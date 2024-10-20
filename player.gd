392d97a1d19488aa85491b775a2d7b1030c731d1
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2024-10-20 08:19:25 -0500

    Add files via upload

--- player.gd
+++ player.gd
@@ -14,8 +14,10 @@ func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
+		speed = 200
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
+		speed = 200
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
@@ -30,7 +32,6 @@ func _process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size)
 	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
-		speed = 200
 	elif velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 
